bf1936851e7f7ffcee6c7302d5526216eab0fae6
khushbu <khushbusoni475@gmail.com>
2023-12-14 14:39:59 +0530

    [khushbu] adding calculator program by using abstract class

--- oops_Level3_PP/Calculator.java
+++ oops_Level3_PP/Calculator.java
@@ -2,6 +2,46 @@ package oops_Level3_PP;
 
 import java.util.Scanner;
 
+abstract class Calculator {
+    abstract void Add(int num1, int num2);
+    abstract void Multiply(int num1, int num2);
+    abstract void Division(int num1, int num2);
+    abstract void Subtract(int num1, int num2);
+}
+class Add extends Calculator
+{
 
+    @Override
+    void Add(int num1, int num2) {
+        System.out.println("Addition of two digits: " + (num1+ num2) );
+    }
+
+    @Override
+    void Multiply(int num1, int num2) {
+        System.out.println("Multiplication of two digits:" + num1*num2);
+    }
+
+    @Override
+    void Division(int num1, int num2) {
+        System.out.println("Division of two digits:" + num1/num2);
+    }
+
+    @Override
+    void Subtract(int num1, int num2) {
+        System.out.println("Subtraction of two digits:" + (num1-num2));
+    }
+
+    public static void main(String[] args) {
+        Add ob = new Add();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two digit");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        ob.Add(num1, num2);
+        ob.Multiply(num1, num2);
+        ob.Division(num1, num2);
+        ob.Subtract(num1, num2);
+    }
+}
 
 
